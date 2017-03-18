e6107525471bf1b5c352e5c83661025b549fe9a6
Skirmantas Januškas <skirmantas.januskas@gmail.com>
2017-03-18 15:50:07 +0200

    Pack and minify css/js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+var cleanCss = require('gulp-clean-css');
 
 gulp.task('connect', function() {
     connect.server({
@@ -8,11 +12,35 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-    gulp.src('./web/*.html');
+    gulp.src('./web/*.html')
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./web/assets'));
+});
+
+gulp.task('pack-css', function () {
+    return gulp.src('./web/assets/style.css')
+        .pipe(concat('style.min.css'))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('./web/assets'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(minify())
+    .pipe(gulp.dest('./web/assets'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['web/*.html'], ['html']);
+    gulp.watch(['./web/*.html'], ['html']);
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/js/**/*.js', ['js']);
+    gulp.watch('./web/assets/style.css', ['pack-css']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'sass', 'js', 'watch']);
+
